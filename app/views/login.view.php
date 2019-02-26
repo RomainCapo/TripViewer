946d920e246b2824caea4ff164f4f50ee52b8634
Romain Capocasale <romain.capocasale@he-arc.ch>
2019-02-26 14:07:39 +0100

    Add bootstrap

--- app/views/login.view.php
+++ app/views/login.view.php
@@ -5,9 +5,16 @@
 <h1>Login</h1>
 
 <form action="login" method="post">
-  <label for="username">Username : </label><input id="username" type="text" name="username" required /><br/>
-  <label for="password">Password : </label><input id="password" type="password" name="password" required /><br/>
-  <input type="submit" value="Login">
+  <label for="username">Username : </label><input class="form-control" id="username" type="text" name="username" required autocomplete="off"/><br/>
+  <div class="form-group">
+    <label for="password">Password : </label><input id="password" type="password" name="password" required autocomplete="off"/><br/>
+  </div>
+  <button type="button" class="btn btn-success" >Login</button>
+
+
+
+
+
 </form>
 
 <?php require('partials/footer.php') ?>
--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -4,9 +4,8 @@
 <head>
 	<title><?= htmlentities($title) ?></title>
     <link rel="stylesheet" type="text/css" href="public/css/my_table_style.css">
-
+		<link rel="stylesheet" type="text/css" href="public/bootstrap/bootstrap.min.css">
 
 </head>
 
 <body>
-    <?php require('nav.php') ?>
