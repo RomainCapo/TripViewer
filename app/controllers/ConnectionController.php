39d427e51dd3cb64ac9039756570874cb91e4ef6
Moulin Vincent <vincent.moulin@he-arc.ch>
2019-03-01 12:16:20 +0100

    add basic registration user

--- app/controllers/ConnectionController.php
+++ app/controllers/ConnectionController.php
@@ -1,7 +1,5 @@
 <?php
 
-require '/models/User.php';
-
 class ConnectionController
 {
     public function index()
@@ -28,14 +26,24 @@ class ConnectionController
     {
         if(isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['confirm_password']))
         {
-            if(User::pseudoAlreadyTaken($_POST['username']))
+            if($_POST['password'] == $_POST['confirm_password'])
             {
-
-            }
+                if(!User::pseudoAlreadyTaken($_POST['username']))
+                {
+                    if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+                    {
+                        if(User::registerUser($_POST['username'], $_POST['email'], $_POST['password']))
+                        {
+                            header('Location: login');
+                            exit();
+                        } else { die('error during register'); }
+                    } else { die('invalid email'); }
+                } else { die("pseudo already taken"); }
+            } else { die('passwords are differents'); }
         }
         else
         {
-            header('Location : register');
+            header('Location: register');
             exit();
         }
     }
--- app/models/User.php
+++ app/models/User.php
@@ -26,11 +26,29 @@ class User extends Model
 
   public static function nbUser()
   {
-      $statement = App::get('dbh')->prepare("SELECT COUNT(*) FROM user");
+      //$statement = App::get('dbh')->prepare("SELECT COUNT(*) FROM user");
   }
 
-  public static function pseudoAlreadyTaken($pesudo)
+  public static function pseudoAlreadyTaken($pseudo)
   {
+      $statement = App::get('dbh')->prepare("SELECT COUNT(id) FROM user WHERE pseudo = :pseudo");
+      $statement->bindParam(':pseudo', $pseudo);
+      $statement->execute();
+      $res = $statement->fetchAll();  
+      
+      return ($res[0][0] == 1) ? true : false;
+  }
 
+  public static function registerUser($pseudo, $email, $password)
+  {
+      $hash_pass = password_hash($password, PASSWORD_DEFAULT);
+
+      $statement = App::get('dbh')->prepare("INSERT INTO user (pseudo, email, password) VALUES (:pseudo, :email, :password)");
+      $statement->bindParam(':pseudo', $pseudo);
+      $statement->bindParam(':email', $email);
+      $statement->bindParam(':password', $hash_pass);
+      $res = $statement->execute();
+
+      return $res;
   }
 }
