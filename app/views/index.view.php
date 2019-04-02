c1b89044a41dd72c0d8a968ca14450714a2522e2
Romain Capocasale <romain.capocasale@he-arc.ch>
2019-04-02 15:23:54 +0200

    fix bug for index page

--- app/views/index.view.php
+++ app/views/index.view.php
@@ -1,25 +1,25 @@
 <?php
     $title = "Home";
     require('partials/header.php');
-    require('partials/nav.php');    
 ?>
 
-<?php if(isset($_SESSION['login'])) { ?>
+<div class="container">
+  <br>
+  <br>
+  <?php if(isset($_SESSION['login'])) { ?>
 
-    <h1>Hi <?php echo unserialize($_SESSION['login'])->getPseudo(); ?> !</h1>
+      <h1>Hi <?php echo unserialize($_SESSION['login'])->getPseudo(); ?> !</h1>
 
-<?php } ?>
+  <?php } ?>
 
-<h3 style="color:grey"><strong>Welcome to TripViewer.</strong></h3>
+  <h3 style="color:grey"><strong>Welcome to TripViewer.</strong></h3>
 
-<br><br>
-<blockquote class="blockquote text-center">
-  <p class="mb-0">"The use of traveling is to regulate imagination by reality, and instead of thinking how things may be, to see them as they are"</p>
-  <footer class="blockquote-footer">Samuel Johnson</footer>
-</blockquote>
-<br><br>
-
-
-
-
-
+  <br><br>
+  <blockquote class="blockquote text-center">
+    <p class="mb-0">"The use of traveling is to regulate imagination by reality, and instead of thinking how things may be, to see them as they are"</p>
+    <footer class="blockquote-footer">Samuel Johnson</footer>
+  </blockquote>
+  <br><br>
+  <a href="login">Login here</a><br>
+  <a href="register">Register here</a>
+</div>
