fa99e715b4a6ad09d59e1a6e8594258bf2c62666
vincent.moulin1 <vincent.moulin@he-arc.ch>
2019-05-23 15:44:59 +0200

    Added verification email before sending

--- app/views/register.view.php
+++ app/views/register.view.php
@@ -31,7 +31,7 @@
                 ?>
 
                 <label for="username">Username</label><input class="form-control" id="username" type="text" name="username" required autocomplete="off" autofocus placeholder="Enter your username"/><br/>
-                <label for="email">Email</label><input class="form-control" id="email" type="text" name="email" required autocomplete="off" placeholder="Enter your email"/><br/>
+                <label for="email">Email</label><input class="form-control" id="email" type="email" name="email" required autocomplete="off" placeholder="Enter your email"/><br/>
                 <label for="password">Password</label><input class="form-control" id="password" type="password" name="password" required autocomplete="off" placeholder="Enter your password"/><br/>
                 <label for="confirm_password">Confirm password</label><input class="form-control" id="confirm_password" type="password" name="confirm_password" required autocomplete="off" placeholder="Confirm your password"/><br/>
               <button type="submit" class="btn btn-success" >Register</button>
--- test/TestDestination.php
+++ test/TestDestination.php
@@ -5,6 +5,9 @@ require_once('./core/bootstrap.php');
 
 use PHPUnit\Framework\TestCase;
 
+/*
+Classe de test qui a pur but de tester les différentes méthodes de la classe Destination
+*/
 final class TestDestination extends TestCase
 {
     /*
@@ -15,6 +18,12 @@ final class TestDestination extends TestCase
       - getDestInfo
     */
 
+    /*
+    Ce test a pour but de confirmer l'ajout d'une destination dans la base de données.
+    On crée un array avec la latitude, longitude et le pays, puis on appelle la méthode saveDestination. Elle retourne l'id.
+    On réalise un assert en comparant le nom de la destination en passant l'id reçu auparavant avec la destination qu'on doit normalement recevoir
+    On retourne l'id, avec le @depends de la function de test testGetDestInfo() situé plu bas, on pourra récupérer cette valeur pour d'autres tests
+    */
     public function testAjoutDb()
     {
       $arrayDest = array("latitude" => 46.95, "longitude" => 6.8333, "country" => "switzerland");
@@ -25,10 +34,20 @@ final class TestDestination extends TestCase
       return $idDest;
     }
 
+    /*
+    Ce test a pour but de tester si une destination est bien dans la base de données.
+    On fait un assert entre l'appel de la méthode destinationInDb() avec le nom d'une destination, celle-ci renverra un boolean.
+    Comme nous avons ajouté précédement boudry à la base de données, celle-ci doit normalemement renvoyée true.
+    */
     public function testDestinatioIsInDb():void
     {
       $this->assertEquals(Destination::destinationInDb("boudry"), true);
     }
+
+    /*
+    On test ici la méthode getDestInfo(), à laquelle on passe l'id que nous avons récupérée du premier test.
+    On effectue ensuite trois asserts, un pour chaque donnée retournée (en effet getDestInfo() retourne un tableau avec des informations comme la latitude ou le pays par exemple).
+    */
 
     /**
      * @depends testAjoutDb
