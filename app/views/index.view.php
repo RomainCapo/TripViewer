b421465c4f420574348f63f331f81e3e65f6a7b1
vincent.moulin1 <vincent.moulin@he-arc.ch>
2019-05-23 15:00:11 +0200

    remove vendor

--- app/views/index.view.php
+++ app/views/index.view.php
@@ -10,14 +10,25 @@
 
 <div class="container">
   <br>
-  <br>
   <?php if(isset($_SESSION['login'])) { ?>
 
-      <h1>Hi <?php echo htmlentities(unserialize($_SESSION['login'])->getPseudo()); ?> !</h1>
+      <h1>Hi <?php echo htmlentities(unserialize($_SESSION['login'])->getPseudo()); ?> !</h1><br>
 
   <?php } ?>
 
-  <h3 style="color:grey"><strong>Welcome to TripViewer.</strong></h3>
+  <!--<h3 style="color:grey"><strong>Welcome to TripViewer.</strong></h3>-->
+
+  <div class="jumbotron">
+    <h1 class="display-4">Welcome to TripViewer.</h1>
+    <p class="lead">You can add a trip, give it a name, added some pictures and see it on a map.</p>
+    <hr class="my-4">
+    <p>You'll will never forget a trip by using this application.</p>
+
+    <?php if(!isset($_SESSION['login'])){
+      echo '<a class="btn btn-success" href="login" role="button">Login here</a><span>&nbsp;&nbsp;&nbsp;&nbsp;</span>';
+      echo '<a class="btn btn-success" href="register" role="button">Register here</a>';
+    }?>
+  </div>
 
   <br><br>
   <blockquote class="blockquote text-center">
@@ -26,10 +37,7 @@
   </blockquote>
   <br><br>
   <div class="row">
-    <?php if(!isset($_SESSION['login'])){
-      echo '<a class="btn btn-success " href="login">Login here</a><span id="space"></span>';
-      echo '<a class="btn btn-success " href="register">Register here</a>';
-    }?>
+    
   </div>
 </div>
 <script type="text/javascript">
